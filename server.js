2c7cfc7c0de9aed3465fcc04e484df7df99bc5ef
SURAJKP <surajkpatil444@gmail.com>
2022-09-20 21:22:51 +0530

    Routing

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
-const http = require('http');
-
 const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("its Middleware");
-    next(); //Allows the request to continue to the next middleware in line.
+app.use('/add-product', (req, res, next) => {
+    res.send('<h1>Add Product</h1>');
 });
 
-app.use((req, res, next) => {
-    console.log("Its another Midlleware");
+app.use('/', (req, res, next) => {
     res.send('<h1>Hello from Express</h1>');
 });
 
-const server = http.createServer(app);
-server.listen(3000, console.log('Server listening at port 3000'));
+app.listen(3000, console.log('Server listening at port 3000'));
