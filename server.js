19e2ed46c283decad98d9318626eae7ba6947c3f
SURAJKP <surajkpatil444@gmail.com>
2022-09-20 21:44:28 +0530

    redirect

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const express = require('express');
 const app = express();
 
 app.use('/add-product', (req, res, next) => {
-    res.send('<h1>Add Product</h1>');
+    res.send('<form action="/products" method=""POST><input type="text" name="title"><button type="submit">Add product</button></form>');
+});
+
+app.use('/products', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
