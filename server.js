6bbfaa6739a8ee317c50ac725e45b6653f37c587
SURAJKP <surajkpatil444@gmail.com>
2022-09-21 20:33:43 +0530

    Express Routing

--- server.js
+++ server.js
@@ -2,19 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+const adminRoute = require('./routes/admin');
+const shopRoute = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: false}));
-
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/products" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>');
-});
-
-app.post('/products', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-    res.send('<h1>Hello from Express</h1>');
-});
+app.use(adminRoute);
+app.use(shopRoute);
 
 app.listen(3000, console.log('Server listening at port 3000'));
