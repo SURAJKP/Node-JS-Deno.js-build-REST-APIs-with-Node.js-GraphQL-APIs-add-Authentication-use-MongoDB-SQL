34264b541cf8060a5c68489fbf4f480d8c75fc94
SURAJKP <surajkpatil444@gmail.com>
2022-09-21 20:48:26 +0530

    Filtering paths

--- routes/admin.js
+++ routes/admin.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
+// admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
     res.send('<form action="/products" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>');
 });
 
+// admin//add-product => POSt
 router.post('/products', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const adminRoute = require('./routes/admin');
 const shopRoute = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(adminRoute);
-app.use(shopRoute);
+app.use('/admin', adminRoute);
+app.use('/shop', shopRoute);
 
 app.use((req, res, next) => {
     res.status(404).send('<h1>404 Page not found</h1>');
