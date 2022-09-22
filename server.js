d474e245c5b5b6f19fd49b5e40ec116b6ab915ea
SURAJKP <surajkpatil444@gmail.com>
2022-09-22 15:40:33 +0530

    Helper function for navigation

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const rootDir = require('./utils/path');
 
 const adminRoute = require('./routes/admin');
 const shopRoute = require('./routes/shop');
@@ -11,7 +12,7 @@ app.use('/admin', adminRoute);
 app.use('/shop', shopRoute);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 })
 
 app.listen(3000, console.log('Server listening at port 3000'));
